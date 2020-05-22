78775286ef51f58fbe0dfd9e84040a89991c00a8
JasmeerSangha <61358666+JasmeerSangha@users.noreply.github.com>
2020-05-22 10:54:23 -0400

    Created earthquake overlay.

--- Mapping_GeoJSON_LineStrings/static/js/logic.js
+++ Mapping_GeoJSON_LineStrings/static/js/logic.js
@@ -1,23 +1,54 @@
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([44.0, -80.0], 2);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: mapbox_key
 });
-// Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: mapbox_key
+});
+
+
+
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+
+
 
 // Accessing the Toronto airline routes GeoJSON URL.
 let torData = "https://raw.githubusercontent.com/JasmeerSangha/Mapping_Earthquakes/master/torontoRoutes.json";
 
+
 // Grabbing our GeoJSON data.
 d3.json(torData).then(function(data) {
     console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data).addTo(map);
+  L.geoJson(data, {color:'orange',
+  onEachFeature: function(feature, layer){
+    layer.bindPopup("<h3>"+ feature.properties.airline + "</h3> <hr> <h3>" + 
+    feature.properties.dst + "</h3>");
+  }
+}).addTo(map);
 });
 
 
+
+
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+	center: [30, 30],
+	zoom: 2,
+  layers: [streets]
+})
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -7,47 +7,48 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satellite = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: mapbox_key
 });
 
 
-// Create the map object with center, zoom level and default layer.
-let map = L.map('mapid', {
-	center: [30, 30],
-	zoom: 2,
-	layers: [streets]
-})
+
+
 // Create a base layer that holds both maps.
 let baseMaps = {
   Street: streets,
-  Dark: dark
+  Satellite: satellite
 };
 
 
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
 
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/JasmeerSangha/Mapping_Earthquakes/master/majorAirports.json";
-
-
-
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/JasmeerSangha/Mapping_Earthquakes/master/torontoNeighborhoods.json";
 
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
   L.geoJson(data, {
+    color:'black',fillColor:'red', opacity:0.5, weight:1,
     onEachFeature: function(feature, layer){
-    layer.bindPopup("<h3>"+ feature.properties.name + "</h3> <hr> <h3>" + 
-    feature.properties.faa + "</h3>");
-  }
-}).addTo(map);
+      layer.bindPopup("<h3>"+ feature.properties.AREA_NAME + "</h3>");
+    }
+  }).addTo(map);
 });
 
 
+
+
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+	center: [43.7, -79.3],
+	zoom: 10,
+  layers: [satellite]
+})
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
